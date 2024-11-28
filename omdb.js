8a5336506e8192dd4c014eef6b6e07e269773b3f
Pablo <pablopemar11@gmail.com>
2024-11-28 12:51:42 +0100

    primera subida

--- omdb.js
+++ omdb.js
@@ -1,16 +1,3 @@
-/*baa35bb8 
-t = encontrar 1 peli
-s = todas las peliculas
-http://www.omdbapi.com/?apikey=baa35bb8&s=star
-
-
-img.addEventListener("click",detalle);
-img.idpeli = pelicula.imdbID;
-function detalle(){
-e.target.idpeli;
-url = ""http://www.omdbapi.com/?apikey=baa35bb8&s="+idpeli;
-}
-*/ 
 let paginanueva = 2;
 window.onload = () =>{
     document.getElementById("btn").addEventListener("click", peticionAJAXmoderna);
@@ -24,10 +11,6 @@ function peticionAJAXmoderna(){
     let milista = document.getElementById("lista");
     milista.innerHTML = "";
     
-      //  let npeliculas = document.getElementById("numeropeliculas");
-      //  npeliculas.innerText = datosRecibidos.totalResult;
-      //  document.getElementById("numeropeliculas").appendChild(npeliculas);
-
     for (pelicula of datosRecibidos.Search) {
         let li = document.createElement("li");
         li.innerHTML = `${pelicula.Title} ${pelicula.Year}`;
@@ -65,10 +48,3 @@ function cargarMas(){
     console.log(datosRecibidos)
   }).catch((err)=> console.error("error",err));
 }
-/*
-img.addEventListener("click",info);
-img.idpeli = pelicula.imdbID;
-function info(e){
-  e.target.idPelicula;
-  url = "http://www.omdbapi.com/?apikey=baa35bb8&s="+idpeli;
-}*/
